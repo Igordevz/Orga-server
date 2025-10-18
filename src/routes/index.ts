0e97912541fe0d7e60af2dd79f8033981e957c20
Igor Santos <igorsantos@MacBook-Air-de-Igor.local>
2025-10-18 16:06:30 -0300

    register router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import type { FastifyInstance } from "fastify";
 import userRouter from "./user";
+import { workSpaceRouter } from "./workspace";
 
 export default function RouterIndex(app: FastifyInstance) {
   const router = app;
 
   router.register(userRouter);
+  router.register(workSpaceRouter);
+
   router.get("/", () => {
     return "Hello World!";
   });
